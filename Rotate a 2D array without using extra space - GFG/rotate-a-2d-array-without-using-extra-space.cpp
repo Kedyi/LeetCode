9134771f4f2a1505de6527f1bccd55918c13b690
Komal <93571457+Kedyi@users.noreply.github.com>
2022-06-21 19:47:13 +0530

    Added solution - LeetHub

--- Rotate a 2D array without using extra space - GFG/rotate-a-2d-array-without-using-extra-space.cpp
+++ Rotate a 2D array without using extra space - GFG/rotate-a-2d-array-without-using-extra-space.cpp
@@ -9,40 +9,43 @@ using namespace std;
 class Solution{
 public:	
 	
-	void rotateMatrix(vector<vector<int>>& matrix, int n) {
-	   //  for(int i=0;i<n;i++){
-	   //      for(int j=i;j<n;j++){
-	   //          int temp=arr[i][j];
-	   //          arr[i][j]=arr[j][i];
-	   //          arr[j][i]=temp;
-	   //      }
-	   //      for(int i=0;i<n;i++){
-	   //          int li=0;
-	   //          int ri=n-1;
-	   //          while(li<ri){
-	   //          int temp=arr[li][i];
-	   //          arr[li][i]=arr[ri][i];
-	   //          arr[ri][i]=temp;
+	void rotateMatrix(vector<vector<int>>& arr, int n) {
+	     for(int i=0;i<n;i++){
+	         for(int j=i;j<n;j++){
+	             int temp=arr[i][j];
+	             arr[i][j]=arr[j][i];
+	             arr[j][i]=temp;
+	         }
+	     }
+	         for(int i=0;i<n;i++){
+	             int li=0;
+	             int ri=n-1;
 	             
-	   //          li++;
-	   //          ri--;
-	   //      }
-	   //      }
-	   //  }
+	             while(li<ri){
+	             int temp=arr[li][i];
+	             arr[li][i]=arr[ri][i];
+	             arr[ri][i]=temp;
+	             
+	             li++;
+	             ri--;
+	         }
+	         
+	     }
+	     }
 
-    for(int i=0; i<matrix.size(); i++)
-    {
-        reverse(matrix[i].begin(), matrix[i].end());
-    }
-    for(int i=0; i<matrix.size(); i++)
-    {
-        for(int j= i+1; j< matrix[i].size(); j++)
-        {
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
-    return;
-	}
+//     for(int i=0; i<matrix.size(); i++)
+//     {
+//         reverse(matrix[i].begin(), matrix[i].end());
+//     }
+//     for(int i=0; i<matrix.size(); i++)
+//     {
+//         for(int j= i+1; j< matrix[i].size(); j++)
+//         {
+//             swap(matrix[i][j], matrix[j][i]);
+//         }
+//     }
+//     return;
+// 	}
 
 };
 
