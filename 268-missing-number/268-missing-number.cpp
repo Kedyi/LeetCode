1d914a70097c0b0e1952e6327cd0af128c7cfdcf
Komal <93571457+Kedyi@users.noreply.github.com>
2022-09-17 13:14:36 +0530

    Time: 70 ms (5.89%), Space: 18.1 MB (20.34%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,7 +3,8 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int i;
-        for(i=0;i<nums.size();i++){
+        int n=nums.size();
+        for(i=0;i<n;i++){
            if(nums[i]!=i){
                return i;
            } 
