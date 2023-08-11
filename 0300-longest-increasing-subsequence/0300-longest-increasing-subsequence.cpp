4f95eb052d5508c43ce32cd5d65024430ec35bfd
Komal <93571457+Kedyi@users.noreply.github.com>
2023-08-11 15:27:30 +0530

    Time: 534 ms (39.23%), Space: 292.3 MB (16.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,24 @@
 class Solution {
     
-int fun(int ind,int prev_ind,int n, vector<int> &nums, vector<vector<int>> &dp){
-    //0->n-1
-    
-    if(ind==n){
-        return 0;
-    }
-    
-    if(dp[ind][prev_ind+1]!= -1) return dp[ind][prev_ind+1];
-    
-    //if i dont want to take the element:
-    int not_take = 0 + fun(ind+1,prev_ind,n, nums,dp);
-    
-    int take=0;
-    if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
-        take = 1 + fun(ind+1,ind,n, nums,dp);
-    }
-    return dp[ind][prev_ind+1] = max(take,not_take);
-}
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-       return fun(0,-1,n,nums,dp);
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev_ind = ind-1;prev_ind>=-1;prev_ind--){
+                
+                int not_take = 0 + dp[ind+1][prev_ind+1];
+    
+                int take=0;
+                if(prev_ind == -1 || nums[ind]>nums[prev_ind]){
+                    take = 1 + dp[ind+1][ind+1];
+                }
+                dp[ind][prev_ind+1] = max(take,not_take);
+                
+            }
+        }
+        
+       return dp[0][-1+1];
     }
 };
