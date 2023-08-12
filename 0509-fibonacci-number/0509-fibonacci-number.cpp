3cc88419e50fa32bb807b01a9dd1e037dfc3a6d4
Komal <93571457+Kedyi@users.noreply.github.com>
2023-08-12 19:45:01 +0530

    Time: 12 ms (43.03%), Space: 5.9 MB (90.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,28 +1,7 @@
 class Solution {
-    
-private:
-    int fibonacciAtn(int n,vector<int> &dp){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = fibonacciAtn(n-1, dp)+fibonacciAtn(n-2, dp);
-    }
 public:
     int fib(int n) {
-        
-        if(n==0)  return 0;
-        
-        int prev2 = 0;
-        int prev = 1;
-        
-        for(int i=2;i<n+1;i++){
-            int curr= prev+prev2;
-            
-            prev2=prev;
-            prev=curr;
-        }
-        
-        return prev;
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
