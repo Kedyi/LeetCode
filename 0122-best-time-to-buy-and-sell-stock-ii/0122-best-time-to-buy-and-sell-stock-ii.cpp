6e344f0487469311f6b7d648c519aa94127c9253
Komal <93571457+Kedyi@users.noreply.github.com>
2023-08-12 17:34:03 +0530

    Time: 3 ms (94.59%), Space: 15.2 MB (31.42%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -29,7 +29,31 @@ private:
     }
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return max_profit(0,1,prices,dp); //1 means we are allowed to buy
+        vector<vector<int>> dp(prices.size()+1,vector<int>(2,0));
+        
+        //changing parameters : buy,ind
+        for(int ind = prices.size()-1;ind>=0;ind--){
+            for(int buy = 1;buy>=0;buy--){
+                int profit = 0;
+                if(buy){
+
+                    int profit_when_take = -prices[ind] + dp[ind+1][0];
+                    int profit_when_not_take = 0 + dp[ind+1][1]; 
+
+                    profit = max(profit_when_take,profit_when_not_take);
+                }
+                else{
+                    //sell
+                    int profit_when_sell = prices[ind] + dp[ind+1][1];
+                    int profit_when_not_sell = 0 + dp[ind+1][0];
+
+                    profit = max(profit_when_sell,profit_when_not_sell);
+                }
+
+                dp[ind][buy] = profit; 
+            }
+        }
+        
+        return dp[0][1]; //1 means we are allowed to buy
     }
 };
