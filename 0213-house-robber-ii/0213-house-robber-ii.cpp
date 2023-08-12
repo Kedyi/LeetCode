91d0d3bd324e9c4c7f17dcc7e6352e6d8e02e8e4
Komal <93571457+Kedyi@users.noreply.github.com>
2023-08-13 01:17:24 +0530

    Time: 3 ms (40.52%), Space: 7.8 MB (77.60%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
-    
- int rob1(vector<int>& nums) {
-        int prev=nums[0];
-        int prev2=0;
-        //base case 0
+int rob1(vector<int> &nums){
+    int prev=nums[0];
+        int prev2 = 0;
+        //0->n-1
+        for(int ind = 1;ind<nums.size();ind++){
+            int decide_to_rob = nums[ind];
+            if(ind>1) decide_to_rob +=  prev2;
+            int decide_not_to_rob = 0 + prev;
+
+            int curr = max(decide_to_rob,decide_not_to_rob);
+            
+            prev2=prev;
+            prev = curr;
+        }
         
-    for(int ind=1;ind<nums.size();ind++){
-        int nottake = 0 + prev;
-        int take = nums[ind];
-        if(ind>1) take += prev2;
-
-        int curr = max(take,nottake); 
-
-        prev2=prev;
-        prev=curr;
-    }
-       return prev;
+        return prev;
 }
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==1) return nums[0];
+        if(nums.size()==1){
+            return nums[0];
+        }
         vector<int> temp1;
         vector<int> temp2;
         for(int i=0;i<nums.size();i++){
-            
-            if(i!=0)  temp1.push_back(nums[i]);
-            if(i!=nums.size()-1) temp2.push_back(nums[i]);
+            if(i!=0){
+                temp1.push_back(nums[i]);
+            }
+            if(i!=nums.size()-1){
+                temp2.push_back(nums[i]);
+            }
         }
-        
         return max(rob1(temp1),rob1(temp2));
     }
 };
