f7b52fe1fe8337f9ea661108d45aa2255ff27c9f
Komal <93571457+Kedyi@users.noreply.github.com>
2023-09-01 16:06:24 +0530

    Time: 3 ms (89.82%), Space: 7.8 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -7,7 +7,7 @@ public:
         if(n==0){
             return ans;
         }
-        for(int i=0;i<=n;i++){
+        for(int i=1;i<=n;i++){
             if(i%2==0){
                 ans[i]=ans[i/2];
             }
