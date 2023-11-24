b9448e6c7dff553974c4ed8698f70ca1abc489a4
Komal <93571457+Kedyi@users.noreply.github.com>
2023-11-24 09:03:34 +0530

    Time: 95 ms (46.99%), Space: 53.8 MB (26.00%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -2,27 +2,10 @@ class Solution {
 public:
     int maxCoins(vector<int>& piles) {
         //8 7 4 2 2 1
-        vector<int> temp;
-        sort(piles.begin(),piles.end(),greater<>());
-        int i=0;
-        int j=piles.size()-1;
-        while(i<piles.size() && j>=0 && i<j){
-            temp.push_back(piles[i]);
-            i++;
-            temp.push_back(piles[i]);
-            i++;
-            temp.push_back(piles[j]);
-            j--;
-        }
-        
-        // for(int k=0;k<temp.size();k++){
-        //     cout<<temp[k];
-        // }
-        
-       // 8 7 1 4 2 2
-        int ans=0;  
-        for(int i=1;i<temp.size();i+=3){
-            ans+=temp[i];
+        sort(piles.begin(),piles.end());
+        int ans=0;
+        for(int i=piles.size()/3;i<piles.size();i+=2){
+            ans+=piles[i];
         }
         return ans;
     }
