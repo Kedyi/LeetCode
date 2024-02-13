68658cab4b548abd57b75f5703f24b8537cc4691
Komal <93571457+Kedyi@users.noreply.github.com>
2024-02-13 07:07:52 +0530

    Time: 12 ms (15.71%), Space: 12.6 MB (17.47%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,62 +1,32 @@
 class Solution {
     
-private:
-    int fun(int row, int col, vector<vector<int>> &grid,vector<vector<int>> &dp){
-        
-        //base
-        if(row==0 && col==0) return grid[0][0];
-        
-        //memoization
-        if(dp[row][col]!=-1) return dp[row][col];
-        
-        //came from up
-        int up = INT_MAX;
-        if(row>0) 
-            up = grid[row][col] + fun(row-1,col,grid,dp);
-        //came from left
-        int left = INT_MAX;
-        if(col>0)
-            left = grid[row][col] + fun(row,col-1,grid,dp);
-        
-        return dp[row][col] = min(up,left);
-    }
+long long fun(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &dp){
     
+    //if reached dest no need to call function
+    if(row==0 && col==0) return grid[0][0];
+    
+    if(row<0 || col<0) return INT_MAX;
+    
+    //memo
+    if(dp[row][col]!=-1) return dp[row][col];
+    
+    //f(i,j): give min cost to reach this point
+    long long up = grid[row][col]+fun(row-1,col,grid,dp);
+    long long left = grid[row][col]+fun(row,col-1,grid,dp);
+    
+    return dp[row][col] = min(up,left);
+}
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        // express prob in form of indexes
+        int n=grid.size()-1;
+        int m=grid[0].size()-1;
+        //-1 is coz we are sending indexes
         
-        vector<vector<int>> dp(n,vector<int>(m,0));
         
-        // //base case: start point
-        // dp[0][0] = grid[0][0];
+        //dp size given so no -1;
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
         
-        //0->n/m
-    
-        for(int row=0; row<n ; row++){
-            
-            for(int col=0; col<m; col++){
-                
-                //base case
-                if(row==0 && col==0) dp[row][col] = grid[0][0];
-                
-                //came from up
-                else{
-                    int up = INT_MAX;
-                    if(row>0) 
-                        up = grid[row][col] + dp[row-1][col];
-
-                    //came from left
-                    int left = INT_MAX;
-                    if(col>0)
-                        left = grid[row][col] + dp[row][col-1];
-
-                    dp[row][col] = min(up,left);
-                }
-                
-            }
-        }
-        
-        return dp[n-1][m-1];
+        return fun(n,m,grid,dp);
     }
 };
