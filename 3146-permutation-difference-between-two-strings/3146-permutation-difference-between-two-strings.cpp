32d17a208631f1531a8b0508d9daa96ae71cddd6
Komal <93571457+Kedyi@users.noreply.github.com>
2024-05-12 16:51:16 +0530

    Time: 6 ms (60.00%), Space: 10.1 MB (60.00%) - LeetHub

--- 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.cpp
@@ -5,7 +5,9 @@ public:
         1. s[i] check in t[j] -> abs(i-j): TC:O(n^2) SC:O(1)
         2. hashmap - > store index with element-> find the element ->take index and abs(i-j): TC:O(2n), SC: O(2n)
         */
-        //1
+        /*
+        1.
+        
         int ans=0;
         int n = s.size();
         
@@ -17,5 +19,25 @@ public:
            } 
         }
         return ans;
+        
+        */
+        
+        //2
+        int ans=0;
+        int n = s.size();
+        unordered_map<char, int> mpp;
+        for(int i=0;i<n;i++){
+            mpp.insert({s[i],i});
+        }
+        
+        // for(auto &x: mpp){
+        //   cout<<x.first<<" "<<x.second<<endl;
+        // }
+        
+        for(int i=0;i<n;i++){
+           // cout<<mpp[t[i]]<<" "<<i<<endl;
+            ans += abs(mpp[t[i]]-i);
+        }
+       return ans; 
     }
 };
