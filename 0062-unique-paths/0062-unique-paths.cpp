721e0c55166849b6044415aea3b22425c8a70934
Komal <93571457+Kedyi@users.noreply.github.com>
2024-07-31 04:03:03 +0530

    Time: 0 ms (100.00%), Space: 8 MB (7.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,8 +14,26 @@ private:
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m+1, vector<int>(n+1,-1));
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        // dp[0][0]=1;
         
-        return fun(m-1,n-1, dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[0][0]=1;
+                
+                else{
+                    int up=0;
+                    if(i>0)
+                     up = dp[i-1][j];
+                    int left=0;
+                    if(j>0)
+                     left = dp[i][j-1];
+
+                    dp[i][j] = left+up;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 };
