7476ae101121a7496e5c22532c5712ed60f2f1a8
Komal <93571457+Kedyi@users.noreply.github.com>
2024-07-30 23:10:10 +0530

    Time: 431 ms (48.28%), Space: 209.8 MB (44.22%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -32,8 +32,35 @@ public:
         
         int n = prices.size();
         
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3,-1)));
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3,0)));
         
-        return fun(0, 1, 2, prices,n, dp);
+        
+        for(int ind=n-1; ind>=0; ind--){
+            for(int buy=0; buy<=1;buy++){
+                for(int cap=1; cap<=2;cap++){
+                    
+                    int profit;
+        
+                    if(buy){
+                        int take = -prices[ind] + dp[ind+1][0][cap];
+                        int not_take = 0 + dp[ind+1][1][cap];
+
+                        profit = max(take, not_take);
+                    }
+                    else{
+                        //allowed to sell
+                        int take = prices[ind] + dp[ind+1][1][cap-1];
+                        int not_take = 0 + dp[ind+1][0][cap];
+
+                        profit = max(take, not_take);
+
+                    }
+                    
+                    dp[ind][buy][cap] = profit;
+                }
+            }
+        }
+        
+        return dp[0][1][2];
     }
 };
