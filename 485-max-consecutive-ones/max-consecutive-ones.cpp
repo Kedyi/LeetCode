37d18192c9c813aff4d2bc353fae55e4442f36fa
Komal <93571457+Kedyi@users.noreply.github.com>
2026-05-05 11:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (32.90%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -6,13 +6,12 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
                 curr++;
+                mx = max(mx, curr);
             }
             else{
-                mx = max(mx, curr);
                 curr=0;
             }
         }
-        mx = max(mx, curr);
         return mx;
     }
 };
