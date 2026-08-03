a12c14a1b57b82a9211a98a9e043885560471ddb
Komal <93571457+Kedyi@users.noreply.github.com>
2026-08-04 00:38:55 +0530

    Time: 76 ms (57.98%) | Memory: 173 MB (88.10%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,24 +1,23 @@
-
-
 class LRUCache {
 public:
-struct Node{
-    int key, val;
-    Node* next;
-    Node* prev;
-
-    Node(int k, int v){
-        key = k;
-        val = v;
-    }
-};
+    //dll structure with  map
+    struct node{
+        int key, val;
+        node *prev;
+        node *next;
+        node(int k, int v){
+            key=k;
+            val=v;
+        }
+    };
 
     int cap=0;
-    
-    Node *head = new Node(-1,-1);
-    Node *tail = new Node(-1,-1);
-    unordered_map<int, Node*> mpp;
-    
+    //initialised head tail nodes
+    node *head = new node(-1,-1);
+    node *tail = new node(-1,-1);
+    unordered_map<int, node*> m;
+
+
     LRUCache(int capacity) {
         cap=capacity;
         head->next=tail;
@@ -26,50 +25,52 @@ struct Node{
     }
 
     //function to deletenode o(1)
-    void deletenode(Node* n){
-        Node* beforenode = n->prev;
-        Node* afternode = n->next;
+    void deletenode(node *n){
+        node *beforenode = n->prev;
+        node *afternode = n->next;
         beforenode->next=afternode;
-        afternode->prev=beforenode;
+        afternode->prev = beforenode;
     }
 
     //add node after head , after getting used or inserted
-    void addnode(Node* n){
-        Node* n1 = head->next;
-        n->next=n1;
-        n1->prev=n;
+    void addnode(node* n){
+        node* n1 = head->next;
         head->next=n;
+        n->next=n1;
         n->prev=head;
-
+        n1->prev=n;
     }
     
     int get(int key) {
-        if(mpp.find(key)==mpp.end()) return -1;
-
-        Node* address = mpp[key];
+        //if not found
+        if(m.find(key)==m.end()) return -1;
+        //found->get node->get value->move node after head
+        node* address = m[key];
         int ans = address->val;
         deletenode(address);
         addnode(address);
-        mpp[key] = head->next;
+        m[key]=head->next;
         return ans;
     }
     
     void put(int key, int value) {
-        if(mpp.find(key)!=mpp.end()){
-            Node* address=mpp[key];
-            deletenode(address);
-            addnode(address);
-            head->next->val = value;
-        }
-        else{
-            if(mpp.size()==cap){
-                mpp.erase(tail->prev->key);
+        //found key update, put it after head
+        if(m.find(key)!=m.end()){
+            node* add = m[key];
+            deletenode(add);
+            addnode(add);
+            head->next->val=value;
+        }else{
+            //full so remove least used that is before tail
+            if(m.size()==cap){
+                //also delete map value
+                m.erase(tail->prev->key);
+                //delete node
                 deletenode(tail->prev);
             }
-            addnode(new Node(key, value));
-            mpp[key]=head->next;
+            addnode(new node(key,value));
+            m[key]=head->next;
         }
-
     }
 };
 
