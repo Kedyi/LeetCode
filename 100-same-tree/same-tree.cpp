76202a7172353a718e269082be5928bcc619cbd9
Komal <93571457+Kedyi@users.noreply.github.com>
2026-08-07 03:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (78.70%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -16,10 +16,10 @@ public:
 
         if(p==NULL || q==NULL) return false;
 
-        if(p->val==q->val){
-            return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+        if(p->val!=q->val){
+            return false;
         }
 
-        return false;
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
     }
 };
