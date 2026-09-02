6deb533848d66be2da390be08e6385d31b54d44c
Komal <93571457+Kedyi@users.noreply.github.com>
2026-09-03 02:44:04 +0530

    Time: 51 ms (25.98%) | Memory: 52.9 MB (14.21%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -29,9 +29,40 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, INT_MAX));
 
-        int ans = fun(n-1, amount, coins, dp);
+        int ans = INT_MAX;
+
+        //first column->amount==0 base case
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+
+        //first row->last index 0 , can we form using that
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=i/coins[0];
+            }
+        }
+
+
+        for(int ind=1;ind<n;ind++){
+            for(int amt=1;amt<=amount;amt++){
+
+                int take = INT_MAX;
+                if(amt>=coins[ind]){
+                if(dp[ind][amt-coins[ind]]!=INT_MAX)
+                    take = 1+ dp[ind][amt-coins[ind]];
+                }
+                //nottake
+                int nottake = 0 + dp[ind-1][amt];
+
+                dp[ind][amt] = min(take,nottake);
+
+            }
+        }
+
+        ans = dp[n-1][amount];
 
         return ans==INT_MAX?-1:ans;
     }
