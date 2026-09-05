3700c155d015cff2a2feee78961e0cfd70a54ec7
Komal <93571457+Kedyi@users.noreply.github.com>
2026-09-06 01:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (5.20%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -15,7 +15,26 @@ private:
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return fun(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+
+        for(int row=0; row<m; row++){
+            for(int col=0; col<n; col++){
+
+                if(row==0 && col==0){
+                    dp[row][col]=1;
+                    continue;
+                }
+                int up=0;
+                if(row>0)
+                up = dp[row-1][col];
+                int left=0;
+                if(col>0)
+                left = dp[row][col-1];
+                
+                dp[row][col] = up+left;
+
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
