c00a1af21c0d65548cfee95126b063688926e11e
Komal <93571457+Kedyi@users.noreply.github.com>
2026-09-06 03:11:48 +0530

    Time: 843 ms (5.03%) | Memory: 55.1 MB (8.70%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -17,7 +17,23 @@ private:
     }
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return fun(0, nums, dp);
+        vector<int> dp(nums.size(),false);
+
+        int n=nums.size();
+        dp[n-1]=true;
+
+        for(int ind=n-2;ind>=0;ind--){
+           for(int i=0;i<nums[ind];i++){
+            int canjump = nums[ind]-i;
+            if(canjump+ind<n && dp[canjump+ind]==true){
+            bool op = dp[canjump+ind];
+            if(op==true){
+                dp[ind] = true;
+                break;
+            }
+            }
+        }
+        }
+        return dp[0];
     }
 };
