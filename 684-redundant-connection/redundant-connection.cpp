7ea0520c48c8b33bd4d25e5c44ba13f6185e236b
Komal <93571457+Kedyi@users.noreply.github.com>
2026-09-08 02:03:34 +0530

    Time: 12 ms (12.26%) | Memory: 43.3 MB (5.31%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+
+    bool dfs(int u, int v, int parent, vector<int> &vis, vector<vector<int>> &adj){
+        if(u==v){
+            return true;
+        }
+
+        vis[u] = 1;
+
+        for(int neighbour: adj[u]){
+            if(neighbour==parent){
+                continue;
+            }
+
+            if(!vis[neighbour] && dfs(neighbour, v, u, vis, adj)){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        // start building the graph, if two edges are already visited then it will create a cycle
+        int n = edges.size();
+
+        vector<vector<int>> adj(n+1);
+        for(auto &edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            //before creating the edge check if they are connected or not
+            vector<int> vis(n+1,0);
+            if(dfs(u, v, -1, vis, adj)){
+                return edge;
+            }
+
+            //else add to adj list
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        return {};
+    }
+};
